85727d907617aae04c0a05ecfd2a6814f37d45d4
lepeha <nostresko@gmail.com>
2024-10-04 14:00:32 +0400

    help

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
-import { fetchPosts } from '../redux/slices/posts';
+import { fetchPosts, fetchTags } from '../redux/slices/posts';
 
 export const Home = () => {
   const dispatch = useDispatch();
-  const { posts, tags } = useSelector(state => state.posts);
+  const { posts, tags } = useSelector((state) => state.posts);
 
   const isPostsLoading = posts.status === 'loading';
+  const isTagsLoading = tags.status === 'loading';
 
   React.useEffect(() => {
     dispatch(fetchPosts());
+    dispatch(fetchTags());
   }, [dispatch]);
 
   return (
@@ -26,15 +28,14 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid item xs={12} md={8}>
-          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => 
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
             isPostsLoading ? (
               <Post key={index} isLoading={true} />
             ) : (
               <Post
-                key={obj._id}
-                id={obj._id}
+                _id={obj._id}
                 title={obj.title}
-                imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+                imageUrl={"https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"}
                 user={obj.user}
                 createdAt={obj.createdAt}
                 viewsCount={obj.viewsCount}
@@ -42,11 +43,11 @@ export const Home = () => {
                 tags={obj.tags}
                 isEditable
               />
-            )
+            ),
           )}
         </Grid>
         <Grid item xs={12} md={4}>
-          <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
+          <TagsBlock items={tags.items || []} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
               {
