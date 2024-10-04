537131e34c4de48bcc826dfaab12dd6f59e021ca
lepeha <nostresko@gmail.com>
2024-10-04 17:00:42 +0400

    fix

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,26 +3,48 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import { useForm } from "react-hook-form";
 
 import styles from "./Login.module.scss";
+import { Password } from "@mui/icons-material";
 
 export const Login = () => {
+  const { register, handleSubmit, setError, formState: { errors, isValid } } = useForm({
+    defaultValues: {
+      email: '',
+      Password: ''
+    },
+    mode: 'onChange'
+  });
+
+  const onSubmit = (values) => {
+    console.log(values)
+  };
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
+      <form onSubmit={handleSubmit(onsubmit)}>
       <TextField
         className={styles.field}
         label="E-Mail"
-        error
-        helperText="Неверно указана почта"
+        error={Boolean(errors.email?.message)}
+        helperText={errors.email?.message}
+        {...register('email', {required: "Укажите почту"})}
         fullWidth
       />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
+      <TextField className={styles.field} label="Пароль" 
+      error={Boolean(errors.password?.message)}
+      helperText={errors.password?.message}
+      {...register('password', {required: "Укажите пароль"})}
+      fullWidth 
+      />
+      <Button type="submit" size="large" variant="contained" fullWidth>
         Войти
       </Button>
+      </form>
     </Paper>
   );
 };
